183e8d021cef2b4f2d7d221976f9bd4f1b156245
Dhruv Gambhava <108172163+gambhavadhruv@users.noreply.github.com>
2024-06-24 23:31:49 +0530

    Add files via upload

---
+++ Java Program/P15.java
@@ -0,0 +1,13 @@
+public class P15{
+	public static void main(String[] args){
+		
+	String str = "Hello";
+
+	for (int i=0;i<str.length() ;i++ ) {
+		for (int j=0;j<=i ;j++ ) {
+			System.out.print(str.charAt(j));
+		}
+		System.out.println();		
+		}		
+	}
+}
