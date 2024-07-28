4e1747f0d0536fdaeb309adb90b08986f12c13b1
Dhruv Gambhava <108172163+gambhavadhruv@users.noreply.github.com>
2024-07-28 09:24:06 +0530

    Add files via upload

--- Java Program/Dimondpattern.java
+++ Java Program/Dimondpattern.java
@@ -1,11 +1,11 @@
 import java.util.*;
-public class Dimondpattern{
+public class Dimondpattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of rows : ");
         int n = sc.nextInt();
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=n-i; j++){
+            for(int j=n; j>=i; j--){
                 System.out.print(" ");
             }
             for(int j=1; j<=i; j++){
@@ -13,20 +13,27 @@ public class Dimondpattern{
             }
             for(int j=2; j<=i; j++){
                 System.out.print("*");
+            }
+            for(int j=n; j>=i; j--){
+                System.out.print(" ");
             }
             System.out.println();
         }
-
-        for(int i=n; i>=1; i--){
-            for(int j=1; j<=n-i; j++){
+        //---------lower part
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
                 System.out.print(" ");
             }
-            for(int j=1; j<=i; j++){
+            for(int j=n; j>=i; j--){
+                System.out.print("*");
+            }
+            for(int j=n; j>i; j--){
                 System.out.print("*");
             }
             for(int j=2; j<=i; j++){
-                System.out.print("*");
+                System.out.print(" ");
             }
+            
             System.out.println();
         }
     }
