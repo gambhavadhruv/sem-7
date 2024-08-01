4ecad095d9514e4e5fce16a436845d420cd4c2b6
Dhruv Gambhava <108172163+gambhavadhruv@users.noreply.github.com>
2024-08-01 17:18:10 +0530

    Add files via upload

---
+++ Java Program/EmptyDimondpattern.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class EmptyDimondpattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows : ");
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=n; j>=i; j--){
+                System.out.print("*");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int j=2; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int j=n; j>=i; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //---------lower part
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=n; j>=i; j--){
+                System.out.print(" ");
+            }
+            for(int j=n; j>i; j--){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+    }
+}
